24d4d2a64a862daf23df9f5534ae40280dba5f43
AndrewwSalim <andrew_salim@hotmail.co.nz>
2019-05-20 18:12:11 +1200

    Added code for login and register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
+import { Component, Inject, ViewChild } from '@angular/core';
+import { Platform, NavController, NavParams } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
