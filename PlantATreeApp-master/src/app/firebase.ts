a92320cab860b16b87c60f3cc82eda4f47cae8fc
leimoniraikiri <leimoni98@gmail.com>
2019-05-25 14:38:41 +1200

    main menu and specials commit

---
+++ PlantATreeApp-master/src/app/firebase.ts
@@ -0,0 +1,14 @@
+//Removed information so public can not access our firebase
+//Replace information with your own data base info
+
+const config = {
+    apiKey: "AIzaSyD58M0hZJDsZoyHAYEtnTmVeUjgme9anoY",
+    authDomain: "plantatree-e6e69.firebaseapp.com",
+    databaseURL: "https://plantatree-e6e69.firebaseio.com",
+    projectId: "plantatree-e6e69",
+    storageBucket: "plantatree-e6e69.appspot.com",
+    messagingSenderId: "481881911194",
+    appId: "1:481881911194:web:99a5f88f70072fa6"
+  };
+  
+  export default config
